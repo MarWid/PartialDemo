88707b2076b8271eadde1f2f49f56ca4822facf8
widlund_martin <widlund_martin@hotmail.com>
2018-12-12 13:41:22 +0100

    1341

---
+++ View/Controllers/AppController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class AppController : Controller
+    {
+        [HttpGet]
+        [Route("~/")]
+        public ActionResult Index()
+        {
+            List<String> tmp = new List<string>();
+            for (int i = 0; i < 10; i++)
+                tmp.Add(Guid.NewGuid().ToString());
+
+            return View(tmp);
+        }
+    }
+}
+
+
